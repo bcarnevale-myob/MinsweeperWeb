024e4393f79f46081a31654daf9d93d9f84ecce0
bcarnevale-myob <bianca.carnevale@myob.com>
2019-04-03 16:56:37 +1100

    Implement initial reading of minsweeper to web
    The WebReader and WebWriter is set up to display a Game with the board
    size hard coded. Moves are currently taken through the url.

---
+++ src/main/java/com/bcarnevale/minesweeperweb/Controllers/WebWriter.java
@@ -0,0 +1,25 @@
+package com.bcarnevale.minesweeperweb.Controllers;
+
+import Game.IWriter;
+
+public class WebWriter implements IWriter {
+
+    private String buffer;
+
+    public void WebWriter() {
+        this.buffer = "";
+    }
+
+    @Override
+    public void write(String s) {
+        buffer += (s + "\n");
+    }
+
+    public String getBuffer() {
+        return this.buffer;
+    }
+
+    public void flushBuffer() {
+        this.buffer = "";
+    }
+}
---
+++ src/test/java/com/bcarnevale/minesweeperweb/MinesweeperwebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bcarnevale.minesweeperweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinesweeperwebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

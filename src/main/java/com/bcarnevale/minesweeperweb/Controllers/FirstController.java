db5b4121eeb644eedc2e3e481000758fda2a0c8f
bcarnevale-myob <bianca.carnevale@myob.com>
2019-04-05 15:18:11 +1100

    Seperated game setUp from makeAMove
    The logic for setting up a game is in a seperate method and called in
    the path "/".

--- src/main/java/com/bcarnevale/minesweeperweb/Controllers/FirstController.java
+++ src/main/java/com/bcarnevale/minesweeperweb/Controllers/FirstController.java
@@ -1,10 +1,6 @@
 package com.bcarnevale.minesweeperweb.Controllers;
 
-import Field.*;
 import Game.*;
-import MinePlacer.RandomMinePlacer;
-import MinePlacer.RealRandom;
-import com.bcarnevale.minesweeperweb.GameStuff.Writer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +12,19 @@ public class FirstController {
     private WebWriter webWriter;
     private WebReader webReader;
 
+    @RequestMapping("/")
+    public String setUpGame() {
+        webWriter = new WebWriter();
+        webReader = new WebReader();
+        this.game = new Game(webReader, webWriter);
+        game.setUpGame();
+        String bufferContents = webWriter.getBuffer();
+        webWriter.flushBuffer();
+        return bufferContents;
+    }
+
     @RequestMapping("/move/{x}/{y}")
     public String makeAMove(@PathVariable("x") int x, @PathVariable("y") int y) {
-        if(game == null) {
-            webWriter = new WebWriter();
-            webReader = new WebReader();
-            this.game = new Game(webReader, webWriter);
-            game.setUpGame();
-        }
         webReader.setX(x);
         webReader.setY(y);
         game.play();
@@ -31,18 +32,5 @@ public class FirstController {
         webWriter.flushBuffer();
         return bufferContents;
     }
-
-//    @RequestMapping("/")
-//    public String Wawa() {
-//        this.field = new Field(new Size(5,5), new RandomMinePlacer(new Size(5,5), new RealRandom()));
-//        return field.getPlayerField().replace("\n","<br>");
-//    }
-//
-//    @RequestMapping("/move")
-//    public String Wawa2(){
-//        Wawa();
-//        field.revealSquare(new Coordinates(2,2));
-//        return field.getPlayerField().replace("\n","<br>");
-//    }
 
 }
